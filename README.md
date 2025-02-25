0262a2003ac1a0e63d40ecb4f4ccf04f75bb17f4
lordlehomme <109144245+lordlehomme@users.noreply.github.com>
2025-02-25 16:45:26 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This is an open source repository.
+Feel free to copy its contents, even though know that this is being distributed solely for educational purposes. Any commercial use without both written and approved upon request consent is prohibited.
---
+++ source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main (){
+    char mychar{'a'};
+    int myint{ 123 };
+    double mydouble{ 465.78 };
+    std::cout << "The value of a char variable is: " << myint << '\n';
+      std::cout << "THe value of that bs is : " << mydouble << '\n';
+
+}
