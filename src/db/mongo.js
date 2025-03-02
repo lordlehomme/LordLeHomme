87a9b8a86179e6fd383befb339a17f573dbe1c35
lordlehomme <109144245+lordlehomme@users.noreply.github.com>
2025-03-02 13:44:20 +0000

    new file:   src/components/navBar.js 	new file:   src/db/mongo.js

---
+++ src/db/mongo.js
@@ -0,0 +1,22 @@
+// src/db/mongo.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  address: String,
+  orders: [String],
+  chats: [String],
+  userType: String, // ga, ro, re, dv
+  loyaltyLevel: String,
+  amountSpent: Number,
+  amountEarned: Number
+});
+
+const User = mongoose.model('User', userSchema);
+
+mongoose.connect('mongodb://localhost:27017/yourDatabase', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+module.exports = User;
