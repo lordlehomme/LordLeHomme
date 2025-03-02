bd7be864ae53bd9a933f863dd7035d24f248ced3
lordlehomme <109144245+lordlehomme@users.noreply.github.com>
2025-03-02 13:50:56 +0000

    modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
+// webpack.config.js
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js', // Adjust this path to your entry file
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'), // Output directory
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
@@ -12,9 +13,13 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-        },
+          loader: 'babel-loader'
+        }
       },
-    ],
-  },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
 };
